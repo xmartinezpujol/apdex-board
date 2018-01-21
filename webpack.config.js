f0efa10905ef206172e896ff6f72906d36bbf8a3
Xavier Martínez <xavier@xceed.me>
2018-01-21 20:27:46 +0100

    Updated Webpack output + regen bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './assets/js/bundle.js',
+    filename: './assets/bundle.js',
     path: path.resolve(__dirname, './dist'),
   },
   module: {
